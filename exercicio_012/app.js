ad0911ffd7f051eea84342a2840ea3dce6aa4214
Brunno Ferreira <brunnofos@outlook.com>
2024-02-20 13:45:54 -0300

    alteração na navbar

--- exercicio_012/app.js
+++
@@ -1,7 +0,0 @@
-/* ----------------------------------------------------------------------------
-
-Exercício: 012
-Enunciado:
-    Ao clicar no botão "Adicionar", se o input de texto não estiver vazio, adicionar
-    um parágrafo com esse texto por baixo do botão.
----------------------------------------------------------------------------- */
